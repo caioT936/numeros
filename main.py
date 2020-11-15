ef6ff0ecf6586d1c2022046655286232b1265513
Caio Toledo <caiotoledo@Mac-mini-de-Andre.local>
2020-11-15 19:22:33 -0300

    meu primeiro comit

---
+++ main.py
@@ -0,0 +1,12 @@
+from funcoes import soma
+
+def main():
+    a = int(input('valor 1:'))
+    b = int(input('valor 2:'))
+    c = soma(a,b)
+    print(c)
+
+if __name__ == '__main__':
+    main()
+
+    
---
+++ numero_primos.py
@@ -0,0 +1,27 @@
+ 
+def ePrimo(x):
+  y = 2
+  while 1==1:
+    if x == 2 or x == 3:
+      return True
+      break 
+    elif x==1:
+      return False
+      break
+    elif x%y == 0:
+      return False 
+      break
+    elif y==x-1:
+      return True
+      break
+    y+=1
+
+def main():
+  x = int(input('Digite um valo:'))
+  if (ePrimo(x)):
+    print('e primo')
+  else:
+    print('nao e primo')
+
+if __name__ == '__main__':
+    main()
